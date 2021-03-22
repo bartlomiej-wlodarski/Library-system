413b415da9b027c7eceae30440f629e7749884fa
Maciej Włodarczyk <230466@edu.p.lodz.pl>
2021-03-22 15:15:22 +0100

    Task 1 first commit

---
+++ Task1/Task1/Task1/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task1
+{
+    public class Class1
+    {
+        public float calc(float val1, float val2)
+        {
+            return val1 + val2;
+        }
+    }
+}
