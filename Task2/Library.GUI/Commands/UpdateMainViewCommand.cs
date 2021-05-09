ad1220bc088b1fe7f3184146f697c7c458ab07c3
230467 Bartłomiej Włodarski <230467@edu.p.lodz.pl>
2021-05-09 23:16:43 +0200

    Start Updating Main View

---
+++ Task2/Library.GUI/Commands/UpdateMainViewCommand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Library.GUI.ViewModels;
+
+namespace Library.GUI.Commands
+{
+    public class UpdateMainViewCommand : ICommand
+    {
+        private readonly MainViewModel _viewModel;
+
+        public UpdateMainViewCommand(MainViewModel viewModel)
+        {
+            _viewModel = viewModel;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            if (parameter.ToString() == "Clients")
+                _viewModel.SelectedViewModel = new ClientListViewModel();
+            else if (parameter.ToString() == "Books")
+                _viewModel.SelectedViewModel = new BookListViewModel();
+            else
+                _viewModel.SelectedViewModel = new MainViewModel();
+
+        }
+
+    }
+}
---
+++ Task2/Library.GUI/ViewModels/MainViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Library.GUI.Commands;
+
+namespace Library.GUI.ViewModels
+{
+    class MainViewModel : BaseViewModel
+
+    {
+        private BaseViewModel _selectedViewModel;
+
+        public BaseViewModel SelectedViewModel
+        {
+            get => _selectedViewModel;
+            set
+            {
+                _selectedViewModel = value;
+                RaisePropertyChanged(nameof(SelectedViewModel));
+            }
+        }
+
+        public ICommand UpdateViewCommand { get; set; }
+
+        public MainViewModel()
+        {
+            UpdateViewCommand = new UpdateMainViewCommand(this);
+        }
+    }
+}
